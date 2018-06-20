f22ba45e188e95a1a6d7dc1e8336d032130ab112
Abdelhamid Bakhta <abdelhamid.bakhta@gmail.com>
2018-06-20 13:35:57 +0200

    implement functions
    - enum status
    - book
    - free
    - checkAvailability
    - modifiers

--- contracts/working/RoomBookingService.sol
+++ contracts/working/RoomBookingService.sol
@@ -12,6 +12,7 @@ contract RoomBookingService is Whitelist {
     // contract data
     mapping(bytes32 => Room) private rooms;
 
+    // possible status of a room
     enum Status {FREE, BOOKED, LOCKED}
 
     // definition of the Room structure
@@ -22,6 +23,89 @@ contract RoomBookingService is Whitelist {
         address bookedBy; // the address which have booked the room
         uint256 bookedFrom; // timestamp when the booking starts, equals to 0 if not booked
         uint256 bookedUntil; // timestamp when the booking ends, equals to 0 if not booked
+        bool initialized; // utility boolean to check the presence of a room in the rooms mapping
+    }
+
+    constructor() public{
+        addAddressToWhitelist(msg.sender);
+    }
+
+    /**
+    * @dev Add a room
+    */
+    function addRoom(bytes32 _id, uint256 _capacity) public onlyOwner() nonZeroBytes32(_id) nonZeroUint256(_capacity) {
+        // check if a room with same id already added
+        require(!rooms[_id].initialized);
+        rooms[_id] = Room({
+            id : _id,
+            capacity : _capacity,
+            status : Status.FREE,
+            bookedBy : address(0x0),
+            bookedFrom : 0,
+            bookedUntil : 0,
+            initialized : true
+            });
+    }
+
+    /**
+    * @dev Book a room
+    * @param _id the identifier of the room to book
+    * @param _from the timestamp when to start the booking
+    * @param _until the timestamp when to start the booking
+    * The sender of the message must be whitelisted to book a room
+    * The room must be available at this time slot
+    */
+    function book(bytes32 _id, uint256 _from, uint256 _until) public onlyWhitelisted() onlyIfGreater(_from, _until) {
+        // check if the room exists
+        require(rooms[_id].initialized);
+        checkAvailability(rooms[_id], _from, _until);
+        internalBook(rooms[_id], msg.sender, _from, _until);
+    }
+
+    /**
+    * @dev Free a booked room before the end of the booking
+    * Throws if the sender address does not match the bookedBy address
+    */
+    function free(bytes32 _id) public onlyWhitelisted() {
+        // check if the room exists
+        require(rooms[_id].initialized);
+        require(rooms[_id].bookedBy == msg.sender);
+        internalFree(rooms[_id]);
+    }
+
+    /**
+    * Check the availability of a given room at a given time
+    * @param _from the timestamp when to start the booking
+    * @param _until the timestamp when to start the booking
+    * Throws if the room is not available
+    */
+    function checkAvailability(Room room, uint _from, uint _until) internal onlyNotLocked(room.status) {
+        if (!isAvailable(room)) {
+            // check if booking limit reached
+            if (now >= room.bookedUntil) {
+                internalFree(room);
+            } else {
+                revert();
+            }
+        }
+    }
+
+    function isAvailable(Room room) internal pure returns (bool){
+        return room.status == Status.FREE;
+    }
+
+    function internalBook(Room room, address _by, uint256 _from, uint256 _until) internal onlyNotLocked(room.status) {
+        room.status = Status.BOOKED;
+        room.bookedFrom = _from;
+        room.bookedUntil = _until;
+        room.bookedBy = _by;
+    }
+
+    function internalFree(Room room) internal onlyNotLocked(room.status) {
+        room.status = Status.FREE;
+        room.bookedFrom = 0;
+        room.bookedUntil = 0;
+        room.bookedBy = 0x0;
     }
 
 
@@ -41,48 +125,19 @@ contract RoomBookingService is Whitelist {
         _;
     }
 
-    constructor() public{
-        addAddressToWhitelist(msg.sender);
+    /**
+    * @dev Throws if zero
+    */
+    modifier nonZeroBytes32(bytes32 value){
+        require(value != bytes32(value));
+        _;
     }
 
     /**
-    * @dev Book a room
-    * @param _id the identifier of the room to book
-    * @param _from the timestamp when to start the booking
-    * @param _until the timestamp when to start the booking
-    * The sender of the message must be whitelisted to book a room
-    * The room must be available at this time slot
+    * @dev Throws if zero
     */
-    function book(bytes32 _id, uint256 _from, uint256 _until) public onlyWhitelisted() onlyIfGreater(_from, _until) {
-
-    }
-
-    /**
-    * @dev Free a booked room before the end of the booking
-    * Throws if the sender address does not match the bookedBy address
-    */
-    function free(bytes32 _id) public onlyWhitelisted()  {
-        Room room = rooms[_id];
-    }
-
-    /**
-    * Check the availability of a given room at a given time
-    * @param _from the timestamp when to start the booking
-    * @param _until the timestamp when to start the booking
-    * Throws if the room is not available
-    */
-    function checkAvailability(Room room, uint _from, uint _until) internal onlyNotLocked(room.status) {
-
-    }
-
-    function isAvailable(Room room) internal returns (bool){
-        return room.status == Status.FREE;
-    }
-
-    function free(Room room) internal onlyNotLocked(room.status) {
-        room.status = Status.FREE;
-        room.bookedFrom = 0;
-        room.bookedUntil = 0;
-        room.bookedBy = 0x0;
+    modifier nonZeroUint256(uint256 value){
+        require(value != uint256(value));
+        _;
     }
 }
