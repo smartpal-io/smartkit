947cc9063afab9fffdeddff9159b70b0675c5f8c
Karim Taam <ktaam@hotmail.fr>
2018-06-21 23:36:35 +0200

    update traceability contract
    change type of Position struct, add log event and test

--- contracts/traceability/Traceable.sol
+++ contracts/traceability/Traceable.sol
@@ -5,12 +5,12 @@ import "../openzeppelin-solidity/Whitelist.sol";
 /**
  * @title Traceable
  * @dev Helps manage the traceability of a product
- * @dev See the tests Traceability.test.js for specific usage examples.
+ * @dev See the tests Traceable.test.js for specific usage examples.
  */
 contract Traceable is Whitelist {
 
     //product id
-    string id;
+    bytes32 id;
 
     // raw materials used in this product
     address [] private rawMaterials;
@@ -20,13 +20,16 @@ contract Traceable is Whitelist {
 
     // definition of the Position structure
     struct Position {
-        uint256 date;
-        string latitude;
-        string longitude;
+        uint256 timestamp;
+        uint256 latitude;
+        uint256 longitude;
         address sender;
     }
 
-    constructor(string _id) public{
+    event LogRawMaterialAdded(address sender, address rawMaterial);
+    event LogNewPositionAdded(address sender, uint256 _date);
+
+    constructor(bytes32 _id) public{
         id = _id;
         addAddressToWhitelist(msg.sender);
     }
@@ -34,23 +37,26 @@ contract Traceable is Whitelist {
     /**
     * @dev Add a raw material
     */
-    function addRawMaterial(address _rawMaterial) public onlyWhitelisted() nonEmptyAddress(_rawMaterial) {
+    function addRawMaterial(address _rawMaterial) public onlyWhitelisted() nonEmptyAddress(_rawMaterial){
         rawMaterials.push(_rawMaterial);
+        emit LogRawMaterialAdded(msg.sender, _rawMaterial);
     }
 
     /**
     * @dev Add a step
     */
-    function addStep(uint256 _date, string _latitude, string _longitude) public onlyWhitelisted()
-    nonZeroUint256(_date) nonEmptyString(_latitude) nonEmptyString(_longitude) {
+    function addStep(uint256 _timestamp, uint256 _latitude, uint256 _longitude) public onlyWhitelisted() {
         // check if a room with same id already added
+
         historicalPositions.push(Position({
-            date : _date,
+            timestamp : _timestamp,
             latitude : _latitude,
             longitude : _longitude,
             sender : msg.sender
             })
         );
+
+        emit LogNewPositionAdded(msg.sender,_timestamp);
     }
 
 
@@ -59,14 +65,6 @@ contract Traceable is Whitelist {
     */
     modifier nonZeroUint256(uint256 value){
         require(value != uint256(value));
-        _;
-    }
-
-    /**
-    * @dev Throws if empty
-    */
-    modifier nonEmptyString(string value){
-        require(bytes(value).length != 0);
         _;
     }
 
---
+++ test/traceability/Traceable.test.js
@@ -0,0 +1,47 @@
+import expectThrow from "../../node_modules/openzeppelin-solidity/test/helpers/expectThrow";
+import expectEvent from "../../node_modules/openzeppelin-solidity/test/helpers/expectEvent";
+import assertRevert from '../../node_modules/openzeppelin-solidity/test/helpers/assertRevert';
+
+const TraceableMock = artifacts.require('Traceable');
+
+require('chai')
+  .use(require('chai-as-promised'))
+.should();
+
+contract('Traceable', function (accounts) {
+
+  let mock, rawMaterial;
+
+  const [
+    owner,
+    anyone,
+  ] = accounts;
+
+  before(async function () {
+    mock = await TraceableMock.new(1148850588);
+    rawMaterial = await TraceableMock.new(2228110111);
+  });
+
+  context('in normal conditions', () => {
+    it('should be possible to add raw materials when you are in the whitelist', async function () {
+      await expectEvent.inTransaction(
+        mock.addRawMaterial(rawMaterial.address, { from: owner }),
+        'LogRawMaterialAdded'
+      );
+    });
+    it('should be possible to add a new position for a product if you are in the whitelist', async function () {
+      await expectEvent.inTransaction(
+        mock.addStep(991084188,40714224, -73961452, { from: owner }),
+        'LogNewPositionAdded'
+      );
+    });
+  });
+
+  context('in not normal conditions', () => {
+    it('should not be possible to add raw materials when you are not in the whitelist', async function () {
+      await assertRevert(
+        mock.addRawMaterial(rawMaterial.address, { from: anyone })
+      );
+    });
+   });
+});
